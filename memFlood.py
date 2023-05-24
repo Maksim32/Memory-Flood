4f52d6c61f061057d16281aee19c07a838d1c2df
elmoexe <mgonev@gmail.com>
2023-05-24 18:51:01 +0200

    Added readme file
    Added readme file with brief explanation on how to use the program, also added comments in the program to explain how it works.
    
    v 1.1

--- memFlood.py
+++ memFlood.py
@@ -1,10 +1,14 @@
+# v 1.1
+
 import tkinter as tk
 from os import path
 
+# Initializing variables
 cycle = 0
 words = "you are fucked!!"
 size = 0
 
+# Defining the flood function which opens a file called data.txt and writes on it.
 def flood(mbs):
     global size
     with open("data.txt", "w") as f:
@@ -12,10 +16,12 @@ def flood(mbs):
             for x in range(65536):
                 f.write(words)
     print("done!")
+    # Calculate the file size in megabytes
     size = path.getsize("data.txt")
     size = size / (1024 ** 2)
     varSizeLabel.config(text=size)  # Update the varSizeLabel with the calculated file size
 
+# This function fetches the value inputted from the user and stores it in a variable
 def getUsrCycle(event):
     global cycle
     cycle = int(cycle_textbox.get())
@@ -23,10 +29,12 @@ def getUsrCycle(event):
     cycle_textbox.delete(0, tk.END)
     startBtn.config(command=lambda: flood(cycle))
 
+# Creating the main window
 window = tk.Tk()
 window.geometry("230x100")
 window.title("Memory hogger")
 
+# Creating and placing GUI elements
 megabyteLabel = tk.Label(text="Gigabytes:")
 megabyteLabel.grid(column=1, row=1)
 
@@ -46,4 +54,5 @@ varSizeLabel.grid(column=3, row=2)
 escBtn = tk.Button(window, text="Quit", command=exit)
 escBtn.place(x=100, y=50)
 
+# Start the main event loop
 window.mainloop()
