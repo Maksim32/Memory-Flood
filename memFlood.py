8fbbfe730c7d196d60710de80d5e8336002253bb
elmoexe <mgonev@gmail.com>
2023-05-27 11:35:40 +0200

    v 1.1
    Optimised code with multithreading and more detailed explanation with docstrings.

--- memFlood.py
+++ memFlood.py
@@ -1,58 +1,96 @@
 # v 1.1
 
 import tkinter as tk
+import threading
 from os import path
 
-# Initializing variables
-cycle = 0
-words = "you are fucked!!"
-size = 0
+cycle = 0  # Variable to store the cycle value entered by the user
+words = "you are fucked!!"  # Words to be written in the flood
+size = 0  # Variable to store the calculated file size
 
-# Defining the flood function which opens a file called data.txt and writes on it.
+def floodMultithreading(MBS):
+    """
+    Function to start the flood in a separate thread.
+
+    Parameters:
+    - MBS (int): Number of megabytes to flood (multiplied by 2 for the thread)
+
+    This function creates a new thread and calls the `flood` function
+    with the specified number of megabytes.
+    """
+    thread = threading.Thread(target=flood, args=(MBS*2,))
+    thread.start()
+
 def flood(mbs):
+    """
+    Function to perform the flood by writing the words to a file.
+
+    Parameters:
+    - mbs (int): Number of megabytes to flood
+
+    This function writes the specified words to a file repeatedly
+    to perform the flood. The file size is calculated and stored in
+    the `size` variable.
+    """
     global size
     with open("data.txt", "w") as f:
         for i in range(mbs * 1024):
-            for x in range(65536):
+            for x in range(16384):
+                f.write(words)
+                f.write(words)
+                f.write(words)
                 f.write(words)
     print("done!")
-    # Calculate the file size in megabytes
     size = path.getsize("data.txt")
     size = size / (1024 ** 2)
     varSizeLabel.config(text=size)  # Update the varSizeLabel with the calculated file size
 
-# This function fetches the value inputted from the user and stores it in a variable
 def getUsrCycle(event):
+    """
+    Function to handle the user input for the cycle value.
+
+    Parameters:
+    - event: Event object (unused)
+
+    This function retrieves the cycle value entered by the user,
+    updates the `cycle` variable, and triggers the flood when the
+    start button is clicked.
+    """
     global cycle
     cycle = int(cycle_textbox.get())
     print(cycle)
     cycle_textbox.delete(0, tk.END)
     startBtn.config(command=lambda: flood(cycle))
 
-# Creating the main window
-window = tk.Tk()
-window.geometry("230x100")
-window.title("Memory hogger")
+if __name__ == '__main__':
+    window = tk.Tk()
+    window.geometry("230x100")
+    window.title("Memory hogger")
 
-# Creating and placing GUI elements
-megabyteLabel = tk.Label(text="Gigabytes:")
-megabyteLabel.grid(column=1, row=1)
+    # Create a label for the "Gigabytes" text
+    megabyteLabel = tk.Label(text="Gigabytes:")
+    megabyteLabel.grid(column=1, row=1)
 
-cycle_textbox = tk.Entry(window, foreground="white", width=10)
-cycle_textbox.grid(column=1, row=2)
-cycle_textbox.bind('<Return>', getUsrCycle)
+    # Create an entry box for user input
+    cycle_textbox = tk.Entry(window, foreground="white", width=10)
+    cycle_textbox.grid(column=1, row=2)
+    cycle_textbox.bind('<Return>', getUsrCycle)
 
-startBtn = tk.Button(window, text="Start Flood", command=lambda: flood(cycle), height=2)
-startBtn.grid(column=1, row=3)
+    # Create a button to start the flood
+    startBtn = tk.Button(window, text="Start Flood", command=lambda: floodMultithreading(cycle), height=2)
+    startBtn.grid(column=1, row=3)
 
-showSizeLabel = tk.Label(window, text="True file size (mb):")
-showSizeLabel.grid(column=3, row=1)
+    # Create a label to display the calculated file size
+    showSizeLabel = tk.Label(window, text="True file size (mb):")
+    showSizeLabel.grid(column=3, row=1)
 
-varSizeLabel = tk.Label(window, text=size)
-varSizeLabel.grid(column=3, row=2)
+    # Create a label to show the actual file size
+    varSizeLabel = tk.Label(window, text=size)
+    varSizeLabel.grid(column=3, row=2)
 
-escBtn = tk.Button(window, text="Quit", command=exit)
-escBtn.place(x=100, y=50)
+    # Create a button to quit the application
+    escBtn = tk.Button(window, text="Quit", command=exit)
+    escBtn.place(x=100, y=50)
 
-# Start the main event loop
-window.mainloop()
+    # Start the Tkinter event loop
+    window.mainloop()
